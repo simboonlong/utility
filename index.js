f7c1a0078bb448ded6fbc9a3ecf1ea03734ae60f
Sim Boon Long <simboonlong@gmail.com>
2020-09-12 16:04:51 +0800

    chore: organize folder

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { getCookie, setCookie } = require('./cookie');
-const { getViewport } = require('./getViewport');
-const { throttle } = require('./throttle');
+const { getCookie, setCookie } = require('./helper/cookie');
+const { getViewport } = require('./helper/getViewport');
+const { throttle } = require('./helper/throttle');
 
 module.exports = {
   getCookie,
