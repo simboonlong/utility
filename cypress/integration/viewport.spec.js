6fe9163b09cb19dbdd7e07af3125cc2bcc078d35
Sim Boon Long <simboonlong@gmail.com>
2020-11-06 22:36:42 +0800

    fix: test wait

--- cypress/integration/viewport.spec.js
+++ cypress/integration/viewport.spec.js
@@ -3,11 +3,13 @@ describe('viewport', () => {
     const user = cy
     user.visit('/')
 
-    user.viewport(1000, 660)
+    user.viewport(1000, 600)
+    user.wait(200)
     user.findByTestId('vp-w').should('have.text', '1000')
-    user.findByTestId('vp-h').should('have.text', '660')
+    user.findByTestId('vp-h').should('have.text', '600')
 
     user.viewport(1200, 900)
+    user.wait(200)
     user.findByTestId('vp-w').should('have.text', '1200')
     user.findByTestId('vp-h').should('have.text', '900')
   })
