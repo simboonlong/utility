0fd052574d49f056df4e5c84c844f14e21d2875b
Sim Boon Long <simboonlong@gmail.com>
2020-11-06 22:39:46 +0800

    chore: demo ui (#16)

--- cypress/integration/viewport.spec.js
+++ cypress/integration/viewport.spec.js
@@ -4,10 +4,12 @@ describe('viewport', () => {
     user.visit('/')
 
     user.viewport(1000, 600)
+    user.wait(200)
     user.findByTestId('vp-w').should('have.text', '1000')
     user.findByTestId('vp-h').should('have.text', '600')
 
     user.viewport(1200, 900)
+    user.wait(200)
     user.findByTestId('vp-w').should('have.text', '1200')
     user.findByTestId('vp-h').should('have.text', '900')
   })
