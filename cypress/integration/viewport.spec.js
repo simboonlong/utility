c1023c58667ab72cd1da3161098d11c58d984d97
Sim Boon Long <simboonlong@gmail.com>
2020-11-06 22:33:53 +0800

    fix: try 660

--- cypress/integration/viewport.spec.js
+++ cypress/integration/viewport.spec.js
@@ -3,9 +3,9 @@ describe('viewport', () => {
     const user = cy
     user.visit('/')
 
-    user.viewport(1000, 600)
+    user.viewport(1000, 660)
     user.findByTestId('vp-w').should('have.text', '1000')
-    user.findByTestId('vp-h').should('have.text', '600')
+    user.findByTestId('vp-h').should('have.text', '660')
 
     user.viewport(1200, 900)
     user.findByTestId('vp-w').should('have.text', '1200')
