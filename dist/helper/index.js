40a3aa5dff729b7e6f943fcbc5a23239f203d061
Sim Boon Long <simboonlong@gmail.com>
2020-11-12 22:42:15 +0800

    fix: umd-js (#18)

--- dist/helper/index.js
+++
@@ -1,6 +0,0 @@
-export * from './ease.js';
-export * from './cookie.js';
-export * from './getViewport.js';
-export * from './onWindow.js';
-export * from './scrollTo.js';
-export * from './throttle.js';
