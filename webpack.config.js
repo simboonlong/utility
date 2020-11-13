101fcaba85b2b54ff89b5cdf9eba2dcdf6f6b4ac
Sim Boon Long <simboonlong@gmail.com>
2020-11-13 09:39:58 +0800

    fix: build to directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
 
-module.exports = {
-   entry: "./dist/es6/index.js",
-   output: {
-    path: path.resolve(__dirname, "dist/umd"),
-    filename: "utility.min.js",
-    library: {
-      type: "umd",
+module.exports = [
+  {
+    entry: "./dist/es6/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist/umd"),
+      filename: "utility.min.js",
+      library: {
+        type: "umd",
+      },
     },
   },
-};
+  {
+    entry: "./dist/es6/index.js",
+    output: {
+      path: path.resolve(__dirname, "demo"),
+      filename: "utility.min.js",
+      library: {
+        type: "umd",
+      },
+    },
+  }
+];
