3170f8651db65e2e799ae981644ee0314c30c36b
Sim Boon Long <simboonlong@gmail.com>
2020-11-22 00:32:36 +0800

    fix: timing

--- cypress/integration/ease.spec.js
+++ cypress/integration/ease.spec.js
@@ -5,10 +5,10 @@ describe('ease', () => {
 
     user.findByTestId('opacity').should('have.text', '0')
     user.findByTestId('ease-to-opacity').click()
-    user.wait(500)
+    user.wait(750) // easeInOutCubic, decimal 2
     user.findByTestId('opacity').invoke('text').then(parseFloat).should('be.gt', 0.01)
     user.findByTestId('opacity').invoke('text').then(parseFloat).should('be.lt', 0.99)
-    user.wait(500)
+    user.wait(250) // easeInOutCubic, decimal 2
     user.findByTestId('opacity').should('have.text', '1')
   })
 })
