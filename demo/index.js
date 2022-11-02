6716606dd7ce46b83d1fe94b68ae6ee3b8298e30
Sim Boon Long <simboonlong@gmail.com>
2022-11-02 21:12:33 +0800

    chore: deps (#62)

--- demo/index.js
+++ demo/index.js
@@ -1,20 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const fs = require("fs");
+const path = require("path");
 const pug = require("pug");
-const data = require("./demo/data.json");
+const data = require("./data.json");
 
 const options = {
-  basedir: `${__dirname}/demo/`,
+  basedir: `${__dirname}/`,
   pretty: true,
 };
 
-const html = pug.renderFile(`${__dirname}/demo/index.pug`, {
+const html = pug.renderFile(`${__dirname}/index.pug`, {
   ...options,
   ...data,
 });
 
-fs.writeFile(`${__dirname}/public/index.html`, html, (err) => {
+fs.writeFile(path.join(__dirname, "../public/index.html"), html, (err) => {
   if (err) {
     console.error(err);
   }
